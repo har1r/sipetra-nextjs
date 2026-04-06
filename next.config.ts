96745da25d38ff6ae9111fb70e4cf59c3363e7a7
har1r <muftiharir3@gmail.com>
2026-04-06 14:34:59 +0700

    10-change

--- next.config.ts
+++ next.config.ts
@@ -6,11 +6,6 @@ const nextConfig: NextConfig = {
     // Mengabaikan error TypeScript saat build agar deploy sukses
     ignoreBuildErrors: true,
   },
-  // @ts-ignore - Tambahkan ini jika TypeScript tetap komplain tentang properti eslint
-  eslint: {
-    // Mengabaikan peringatan ESLint saat build
-    ignoreDuringBuilds: true,
-  },
   // Pengaturan tambahan untuk gambar jika diperlukan
   images: {
     remotePatterns: [
